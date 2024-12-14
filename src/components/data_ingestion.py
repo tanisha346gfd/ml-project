ea6e10c4d1c06e569fdd6c2483f7d8c5c2f3e282
tanisha346gfd <tanishaagrawal665@gmail.com>
2024-12-14 13:44:44 +0530

    Data transformation is added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,19 +1,21 @@
-import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.getcwd())))
-
+import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', "train.csv")
     test_data_path: str = os.path.join('artifacts', "test.csv")
-    raw_data_path: str = os.path.join('artifacts', "raw_data.csv")
-
+    raw_data_path: str = os.path.join('artifacts', "data.csv")
 
 class DataIngestion:
     def __init__(self):
@@ -22,40 +24,35 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            # Load the dataset
-            df = pd.read_csv('notebook/data/stud.csv')
-            logging.info("Read the dataset as a dataframe")
+            # Ensure correct path formatting
+            df = pd.read_csv(r'notebook\data\stud.csv')
+            logging.info('Read the dataset as dataframe')
 
-            # Create directories if they don't exist
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
 
-            # Save raw data
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
-            logging.info("Saved raw data to artifacts")
 
-            # Split into train and test sets
-            logging.info("Train-test split initiated")
+            logging.info("Train test split initiated")
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
 
-            # Save train and test data
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
-            logging.info("Train-test split completed and data saved to artifacts")
 
-            # Return paths
+            logging.info("Ingestion of the data is completed")
+
             return (
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
             )
-
         except Exception as e:
             raise CustomException(e, sys)
 
-
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
 
-
-
-
+    # Ensure DataTransformation is implemented
+    data_transformation = DataTransformation()
+data_transformation.initiate_data_transformation(train_data,test_data)
+    # Ensure ModelTrainer is implemented
+    
--- src/exception.py
+++ src/exception.py
@@ -1,52 +1,27 @@
 import sys
-import os
-
-# Add the root directory of the project to the module search path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from src.logger import logging
 
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+     file_name,exc_tb.tb_lineno,str(error))
 
-
-
-
-
-
-
-# Function to capture the error details
-def error_message_details(error, error_detail: sys):
-    _, _, exc_tb = error_detail.exc_info()  # Extract traceback information
-    file_name = exc_tb.tb_frame.f_code.co_filename  # Get the filename of the script
-    error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error)
-    )
     return error_message
-
-# Custom exception class
-class CustomException(Exception):
-    def __init__(self, error_message, error_detail: sys):
-        # Call the parent class (Exception) constructor
-        self.error_message = error_message_details(error_message, error_detail)
-        
-    def __str__(self):
-        return self.error_message
-
-if __name__=="__main__":
-    try:
-        a=1/0
-
-    except Exception as e:
-        logging.info('Divide by zero')
-        raise CustomException(e,sys)
-
 
     
 
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
+    
 
 
-
-
-
+        
 
 
 
